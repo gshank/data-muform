37c18ba6c7392826826bd6e20b905ecb2d6e0e1b
Gerda Shank <gerda.shank@gmail.com>
2015-09-06 13:30:12 -0400

    add some timing tests.

---
+++ t/time_hmf.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use HTML::MuForm;
+use Time::HiRes ('gettimeofday', 'tv_interval');
+
+{
+
+   package My::Form;
+   use Moo;
+   use HTML::MuForm::Meta;
+   extends 'HTML::MuForm';
+
+   has '+name'         => ( default  => 'testform_' );
+
+   has_field 'optname' => ( label     => 'First' );
+   has_field 'reqname' => ( required => 1 );
+   has_field 'somename';
+   has_field 'my_selected' => ( type => 'Checkbox' );
+   has_field 'must_select' => ( type => 'Checkbox', required => 1 );
+
+   sub field_list
+   {
+      return [
+         { name => 'fruit', type => 'Select' },
+         { name => 'optname', label => 'Second' },
+      ];
+   }
+
+   sub options_fruit
+   {
+      return (
+         1 => 'apples',
+         2 => 'oranges',
+         3 => 'kiwi',
+      );
+   }
+}
+
+my $start_run = [gettimeofday];
+
+my $index = 0;
+while ( $index < 1000 ) {
+  my $form = My::Form->new;
+  $form->process( params => {} );
+  $index++;
+}
+
+my $end_run = [gettimeofday];
+
+my $elapsed = tv_interval( $start_run, $end_run );
+
+print "Elapsed: $elapsed\n";
+
